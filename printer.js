1fe91d0a465a934fe07b17e6c38fd1d19aeeecba
congthanhnguyen <te304584>
2017-05-24 18:55:16 +0900

    change examples

--- printer.js
+++ printer.js
@@ -109,8 +109,8 @@ Printer.prototype.text = function(content, encoding){
  */
 Printer.prototype.feed = function (n) {
   this.buffer.write(new Array(n || 1).fill(_.EOL).join(''));
-  //return this.flush();
-    return this;
+  return this.flush();
+    //return this;
 };
 
 /**
