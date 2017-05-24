ddb1475289223bec0bf232570ffd128efa0fed7a
congthanhnguyen <te304584>
2017-05-24 19:04:33 +0900

    change examples

--- commands.js
+++ commands.js
@@ -47,7 +47,7 @@ _.HARDWARE = {
  */
 _.CASH_DRAWER = {
     //CD_KICK_2 : '\x1b\x07\x0b\x37\x07'      , // Sends a pulse to pin 2 []0x1b, 0x07, 0x0b, 0x37, 0x07
-    CD_KICK_2 : '\x1b\x07\x0b',
+    CD_KICK_2 : '\x1b\x07\x0b\x37\x07',
     CD_KICK_5 : '\x1b\x70\x01'      , // Sends a pulse to pin 5 []
 
 };
