e2ef306bed21fecd4518103b4598f2c66c256d55
congthanhnguyen <te304584>
2017-05-28 14:40:44 +0900

    add TM-T90.test.js

---
+++ examples/TM-T90.test.js
@@ -0,0 +1,74 @@
+//const escpos = require('escpos');
+const escpos = require('../');
+
+// Select the adapter based on your printer type
+//const device  = new escpos.USB();
+const device  = new escpos.Network('192.168.11.43');
+//const device  = new escpos.Serial('/dev/rfcomm0');
+
+const printer = new escpos.Printer(device);
+
+device.open(function(){
+
+    printer
+        .text("ご利用ありがとうございます3")
+        .text("ご利用ありがとうございます3")
+        .text("ご利用ありがとうございます3")
+        .cashdraw()
+        .cut()
+    //.close()
+    //.cashdraw()
+    //.flush()
+    ;
+
+    setTimeout(function () {
+        printer
+            .text("ご利用ありがとうございます3")
+            .text("ご利用ありがとうございます3")
+            .text("ご利用ありがとうございます3")
+            //.cashdraw()
+            .cut();
+        // .close();
+
+
+        // printer
+        //
+        // .close();
+
+    },5000);
+
+    setTimeout(function () {
+        printer
+            .text("ご利用ありがとうございます3")
+            .text("ご利用ありがとうございます3")
+            .text("ご利用ありがとうございます3")
+            //.cashdraw()
+            .cut()
+            .close();
+
+
+        // printer
+        //
+        // .close();
+
+    },10000);
+
+
+    //printer.cashdraw();
+
+    //printer.cashdraw(5);
+    //printer.close();
+
+});
+
+// setInterval(function () {
+//     printer
+//         .text("ご利用ありがとうございます3")
+//         .text("ご利用ありがとうございます3")
+//         .text("ご利用ありがとうございます3")
+//         //.cashdraw()
+//         .cut();
+//        // .close();
+// },5000);
+
+
